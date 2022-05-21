d9602ef25c7325425a0e32c0111568ef3a7d0a3d
Chris Keele <christhekeele@gmail.com>
2022-05-21 04:17:00 -0400

    Add doctor and prepare for merge to latest.

---
+++ .doctor.exs
@@ -0,0 +1,15 @@
+%Doctor.Config{
+  exception_moduledoc_required: true,
+  failed: false,
+  ignore_modules: [],
+  ignore_paths: [],
+  min_module_doc_coverage: 100,
+  min_module_spec_coverage: 100,
+  min_overall_doc_coverage: 100,
+  min_overall_spec_coverage: 100,
+  moduledoc_required: true,
+  raise: false,
+  reporter: Doctor.Reporters.Full,
+  struct_type_spec_required: true,
+  umbrella: false
+}
