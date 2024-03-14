7fe9bcc327930d76984d4eaa8d01da3debd0fb3e
Chris Keele <christhekeele@gmail.com>
2024-03-14 14:10:47 -0500

    Remove Spec.stream/2

--- lib/matcha/spec.ex
+++ lib/matcha/spec.ex
@@ -148,47 +148,6 @@ defmodule Matcha.Spec do
     end
   end
 
-  @spec stream(t(), Enumerable.t()) :: Enumerable.t()
-  @doc """
-  Produces a `Stream` that filters out and manipulates elements of an `enumerable`.
-
-  Elements of the `enumerable` that match one of the `spec`'s clauses
-  will transformed as instructed.
-  Elements that do not match will be filtered out of the result.
-
-  Always returns a lazy `Stream` enumerable.
-
-  ## Examples
-
-      # FIXME: context stream logic broken, re-enable after fix
-      # iex> require Matcha
-      ...> Matcha.spec do
-      ...>   {amount, tax} when is_integer(amount) and amount < 0 -> {:charge, amount + tax}
-      ...> end
-      ...> |> Matcha.Spec.stream([
-      ...>   {9001, 0},
-      ...>   {-200, -2.50},
-      ...>   {-3, -0.5},
-      ...>   {:error, "bank was offline"},
-      ...>   {100, 0},
-      ...>   {-743, -16.0},
-      ...> ])
-      ...> |> Stream.take(2)
-      ...> |> Enum.to_list
-      [charge: -202.5, charge: -3.5]
-
-
-  ## Note
-
-  This function wraps the `enumerable` in a lazy `Stream`.
-  If the `enumerable` is something you can safely convert
-  to a list without going on forever or loading too much into memory,
-  consider using `run/2` instead, as it is more efficient.
-  """
-  def stream(%__MODULE__{} = spec, enumerable) do
-    Context.stream(spec, enumerable)
-  end
-
   @spec valid?(t()) :: boolean()
   def valid?(%__MODULE__{} = spec) do
     case validate(spec) do
